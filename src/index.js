86e3cc7eb1dc09d45997c7da7cc8716acf5badd2
csnova <chandlersnova@gmail.com>
2023-09-14 13:23:10 -0700

    HTML and CSS Design for Page -Added a CSS page to style -Added HTML with basic outline

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import "./style.css";
-import { addTaskToArray, printMe, task, allTasks } from "./print.js";
 import { compareAsc, format } from "date-fns";
+import {
+  addTaskToArray,
+  printMe,
+  task,
+  allTasks,
+  removeElementsByClass,
+} from "./print.js";
 
 printMe();
 
+addTaskToArray("none", "Shopping", "Need Milk and Eggs", "Low", "12/12/12");
+addTaskToArray("none", "Shopping", "Need Milk and Eggs", "Low", "12/12/12");
+addTaskToArray("none", "Shopping", "Need Milk and Eggs", "Low", "12/12/12");
 addTaskToArray("none", "Shopping", "Need Milk and Eggs", "Low", "12/12/12");
 console.table(allTasks);
--- src/print.js
+++ src/print.js
@@ -1,3 +1,12 @@
+import Spiral from "./images/spiral.jpg";
+import Paper from "./images/notebookPaper.jpg";
+// <a href="https://www.flaticon.com/free-icons/edit" title="edit icons">Edit icons created by Pixel perfect - Flaticon</a>
+import Edit from "./images/edit.png";
+// <a href="https://www.flaticon.com/free-icons/delete" title="delete icons">Delete icons created by Ilham Fitrotul Hayat - Flaticon</a>
+import Delete from "./images/delete.png";
+// <a href="https://www.flaticon.com/free-icons/plus" title="plus icons">Plus icons created by dmitri13 - Flaticon</a>
+import Add from "./images/edit.png";
+
 const allTasks = [];
 
 function printMe() {
@@ -23,4 +32,11 @@ function addTaskToArray(project, title, description, priority, dueDate) {
   allTasks.push(newTask);
 }
 
-export { task, printMe, addTaskToArray, allTasks };
+function removeElementsByClass(className) {
+  const elements = document.getElementsByClassName(className);
+  while (elements.length > 0) {
+    elements[0].parentNode.removeChild(elements[0]);
+  }
+}
+
+export { task, printMe, addTaskToArray, allTasks, removeElementsByClass };
